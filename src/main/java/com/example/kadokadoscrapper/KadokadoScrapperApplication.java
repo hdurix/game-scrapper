be3c232fe01d52bcbb539cf9f047f7229823b8e3
Hippolyte Durix <hdurix@ippon.fr>
2020-03-29 12:19:10 +0200

    Spring initializr

---
+++ src/main/java/com/example/kadokadoscrapper/KadokadoScrapperApplication.java
@@ -0,0 +1,13 @@
+package com.example.kadokadoscrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KadokadoScrapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KadokadoScrapperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kadokadoscrapper/KadokadoScrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kadokadoscrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KadokadoScrapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
